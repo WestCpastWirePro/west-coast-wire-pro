a73b925b6a1380a77c378d63c6914430a096382e
Danie <wirereadypro@gmail.com>
2026-03-09 18:58:20 -0700

    Fix React import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import LandingPage          from './LandingPage.jsx'
 import WestCoastWirePro         from './WestCoastWirePro.jsx'
 import SuccessPage          from './SuccessPage.jsx'
